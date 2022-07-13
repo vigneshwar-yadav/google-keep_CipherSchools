159138070ed67686ca57221215969f87638a2d3f
VIGNESHWAR YADAV <57144160+vigneshwar-yadav@users.noreply.github.com>
2022-07-13 19:51:49 +0530

    Updates

---
+++ precache-manifest.ceb53871af10be2d25a1dae595b4cfc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eebfe753c40de3a3426",
+    "url": "/google-keep_CipherSchools/static/css/main.ba31b69d.chunk.css"
+  },
+  {
+    "revision": "9eebfe753c40de3a3426",
+    "url": "/google-keep_CipherSchools/static/js/main.9eebfe75.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/google-keep_CipherSchools/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "f3170c7b161f397c88b0",
+    "url": "/google-keep_CipherSchools/static/js/2.f3170c7b.chunk.js"
+  },
+  {
+    "revision": "443bfa54fd6f436e31c8",
+    "url": "/google-keep_CipherSchools/static/js/runtime~main.443bfa54.js"
+  },
+  {
+    "revision": "7bac41de5caf20b0453cafea6138def9",
+    "url": "/google-keep_CipherSchools/index.html"
+  }
+];
